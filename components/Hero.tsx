e8bd0e8592dc54d04eaf355ec8f6d920bd6984c4
adugbo victory <adugbovictory@gmail.com>
2024-03-19 17:37:08 +0100

    favicon

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,10 @@ const Hero = () => {
   return (
     <div className='hero'>
     
-      
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
           Bespoke Tailoring <br />
