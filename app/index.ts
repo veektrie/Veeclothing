1b02dd691fceb261687559a5864ae60dbc43949a
Adugbo Victory <Adugbovictory@gmail.com>
2026-04-21 10:08:52 +0100

    feat: initialize base styles, root layout, and hero section component

--- app/index.ts
+++ app/index.ts
@@ -12,10 +12,6 @@ const styles = {
     flexStart: 'flex justify-start items-start',
     flexEnd: 'flex justify-end',
     navPadding: 'pt-[98px]',
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">,
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">,
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">,
-<link rel="manifest" href="/site.webmanifest">,
   
     // hero section
     heroHeading:
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,17 @@ import Footer from "@/components/Footer";
  
 export const metadata = {
   title: "Bespoke Tailoring | Vee Clothing Company - Premium Fit & Style",
-  description: "Luxurious Minimalist Kaftans | Modern, Effortless Style | Vee Clothing Company |Discover expertly crafted suits with contemporary cuts.Elevate your look with our selection of premium fabrics and personalized tailoring.| Make a lasting impression with our custom corporate branding solutions.Elevate your company's image with apparel, promotional items, and expert design."
+  description: "Luxurious Minimalist Kaftans | Modern, Effortless Style | Vee Clothing Company |Discover expertly crafted suits with contemporary cuts.Elevate your look with our selection of premium fabrics and personalized tailoring.| Make a lasting impression with our custom corporate branding solutions.Elevate your company's image with apparel, promotional items, and expert design.",
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
