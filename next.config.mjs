724d4b7625dcdcd0ce8df6abd7c3a16bbc37e499
Adugbo Victory <Adugbovictory@gmail.com>
2026-04-29 20:25:09 +0100

    chore: update Next.js to 15.1.6 and integrate styled-components with compiler support

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-    typescript: {
+  compiler: {
+    styledComponents: true,
+  },
+  typescript: {
     ignoreBuildErrors: true,
   },
   images: {
